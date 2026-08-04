337c3ee6ec78b7cf138bb0df96e84019bc48e758
ezekiel752 <odoruyiezekiel7@gmail.com>
2026-08-04 17:13:50 +0100

    Added the home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import AnnouncementBar from "../components/AnnouncementBar";
+import Navbar from "../components/Navbar";
+import TrustBar from "../components/TrustBar";
+
+const Home = () => {
+  return (
+    <div>
+      <AnnouncementBar />
+      <Navbar />
+      <TrustBar />
+    </div>
+  );
+};
+
+export default Home;
